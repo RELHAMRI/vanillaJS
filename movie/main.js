78a8320d375fde8d0abdd47e714854e325960b54
RELHAMRI <elh.radouane@gmail.com>
2024-10-10 15:00:03 +0200

    mise à jour exo js

---
+++ movie/main.js
@@ -0,0 +1,15 @@
+let input = document.querySelector('input');
+let button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+
+})
+
+
+let searchMovie = async() => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
